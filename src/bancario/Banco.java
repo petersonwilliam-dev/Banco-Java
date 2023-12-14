347931f9e840900b2411b30977bd1882650d2bf8
Peterson William <petersondev.profissional@gmail.com>
2023-12-14 10:09:37 -0300

    Adicionando função para adicionar e excluir contas

--- src/bancario/Banco.java
+++ src/bancario/Banco.java
@@ -8,6 +8,31 @@ public class Banco {
         this.contas = new ArrayList<>();
     }
 
+    public void adicionarConta(Conta conta) {
+        List listaContas = getContas();
+        listaContas.add(conta);
+
+        setContas(listaContas);
+
+        System.out.println("======================");
+        System.out.println("Olá, "+conta.getUsuario().getNome()+", sua conta foi criada com sucesso!");
+        System.out.println("O número da sua conta é "+conta.getNumeroConta()+" e o número da agência é "+conta.getNumeroAgencia());
+        System.out.println("======================");
+    }
+
+    public void excluirConta(Conta conta) {
+        List listaContas = getContas();
+
+        if (!listaContas.isEmpty()) {
+            listaContas.remove(conta);
+            System.out.println("======================");
+            System.out.println("Pronto, "+conta.getUsuario().getNome()+"! Sua conta foi removida com sucesso!");
+            System.out.println("======================");
+        } else {
+            System.out.println("Não há contas cadastradas");
+        }
+    }
+
     public List<Conta> getContas() {
         return contas;
     }
